6519a7d88a1e1c79b31ddd09499f6f2a5ace0524
Antoine Jonquet <antoine.jonquet@univ-reims.fr>
2023-11-24 16:43:27 +0100

    Mise en cache automatique des ressources de l’application

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,9 @@
-const STATIC_CACHE_NAME = "todosApp-static.v2";
+import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching'
 
-const addResourcesToCache = async (resources) => {
-  const cache = await caches.open(STATIC_CACHE_NAME);
-  await cache.addAll(resources);
-};
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    addResourcesToCache(self.__WB_MANIFEST.map(asset => asset.url)),
-  );
-});
+cleanupOutdatedCaches()
+
+precacheAndRoute(self.__WB_MANIFEST);
 
 
 // background.css
@@ -49,38 +43,6 @@ self.addEventListener("fetch", async (event) => {
   }
   event.respondWith(networkWithCacheFallback(event.request));
 });
-
-
-// Cache first, 
-const cacheFirst = async (request) => {
-  const cache = await caches.open(STATIC_CACHE_NAME);
-  const responseFromCache = await cache.match(request);
-  if (responseFromCache) {
-    return responseFromCache;
-  }
-  return fetch(request);
-};
-self.addEventListener("fetch", (event) => {
-  event.respondWith(cacheFirst(event.request));
-});
-
-
-const deleteCache = async (key) => {
-  await caches.delete(key);
-};
-
-const deleteOldCaches = async () => {
-  const cacheKeepList = [STATIC_CACHE_NAME];
-  const keyList = await caches.keys();
-  const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
-  await Promise.all(cachesToDelete.map(deleteCache));
-};
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(deleteOldCaches());
-  self.clients.claim();
-});
-
 
 
 self.skipWaiting();
--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
     VitePWA({
       strategies: 'injectManifest',
       manifest,
+			includeAssets: 'icons/**/*',
     }),
   ],
 })
