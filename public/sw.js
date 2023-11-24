bd4bab1ab8955312f7a6b5bb73796a6b0443e825
Antoine Jonquet <antoine.jonquet@univ-reims.fr>
2023-11-24 16:06:11 +0100

    Migration du manifest vers la config de vitePWA

--- public/sw.js
+++ public/sw.js
@@ -7,21 +7,7 @@ const addResourcesToCache = async (resources) => {
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    addResourcesToCache([
-      "/",
-      "/index.html",
-      "/manifest.json",
-      "/js/registerSw.js",
-      "/js/app.js",
-      "/css/style.css",
-      "/icons/apple-touch-icon-180x180.png",
-      "/icons/favicon.ico",
-      "/icons/icon-512x512.png",
-      "/icons/maskable-icon-512x512.png",
-      "/icons/pwa-192x192.png",
-      "/icons/pwa-512x512.png",
-      "/icons/pwa-64x64.png",
-    ]),
+    addResourcesToCache(self.__WB_MANIFEST.map(asset => asset.url)),
   );
 });
 
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+import manifest from './src/manifest.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +14,9 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    VitePWA({
+      strategies: 'injectManifest',
+      manifest,
+    }),
   ],
 })
